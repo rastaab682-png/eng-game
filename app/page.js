dbe5fc39b6f4905d4350b8f2d7f86b2ec1f24c87
rastaab682-png <rastaab682@gmail.com>
2025-11-13 17:22:01 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
-      <iframe
-        src="/rastaab682-png/eng-game/index.html"
-        style={{
-          width: '100%',
-          height: '100%',
-          border: 'none',
-          display: 'block',
-        }}
-        title="Eng Game"
-      ></iframe>
-    </div>
-  );
-}
+
